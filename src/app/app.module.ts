124b459eebf78d9b9aa868a21a049836fcab3002
Michael Tran <michaelhuytran@gmail.com>
2017-10-17 21:08:50 -0700

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material'
 
 import { AppComponent } from './app.component';
+import { FeaturesComponent } from './features/features.component';
+import { PopularComponent } from './popular/popular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeaturesComponent,
+    PopularComponent
   ],
   imports: [
     BrowserModule,
