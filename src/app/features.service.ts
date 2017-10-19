74de17625baac89b6175518feca3680712beb54a
Michael Tran <michaelhuytran@gmail.com>
2017-10-19 09:20:19 -0700

    .

--- src/app/features.service.ts
+++ src/app/features.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@angular/core';
 
+
+interface  IFeature {
+  name: string;
+  number: string;
+}
+
 @Injectable()
 export class FeaturesService {
 
   constructor() { }
 
+  getFeatures() {
+    const features: IFeature[] = [
+      {
+        name: 'Call RideShare',
+        number: '131212341234123'
+      },
+      {
+        name: 'Weather',
+        number: '131212341234123'
+      },
+      {
+        name: 'Mary',
+        number: '131212341234123'
+      },
+      {
+        name: 'Call RideShare',
+        number: '131212341234123'
+      }
+
+    ];
+    return features;
+  }
 }
